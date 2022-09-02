ee2a3d4f7539e4e14dbb7dd5c4c5746f2b5d41d3
txiangyang <xiangyangtang@outlook.com>
2022-09-02 11:50:05 +0800

    add history for arch post installation reference

---
+++ archlinux/postInstallation.sh
@@ -0,0 +1,724 @@
+  #vi /etc/locale.conf
+yay -S reflector
+
+yay -S intel-media-sdk intel-media-driver intel-compute-runtime libva-mesa-driver 
+
+
+#  ls
+#  cd ~
+#  nvim .zshrc
+#  cd~
+#  cd ~
+#  ls
+#  pwd
+#  rm -rf ohmyzsh
+#  ls
+#  tmux
+#  paru jellyfin-ffmpeg5
+#  ls
+#  fdisk -l
+#  sudo fdisk -l
+#  sudo mount /dev/nvme0n1p7 /mnt/manjaro
+#  sudo vi /etc/fstab
+#  ls
+#  clear
+#  sudo nvim /etc/fstab
+#  sudo mount /dev/nvme0n1p6 /mnt/manjaro
+#  sudo nvim /etc/fstab
+#  ls
+#  cd ~
+#  ls
+#  ls -al
+#  ls /bin
+#  ls
+#  sudo fdisk -l
+#  mount /dev/mmcblk0p1 /mnt/sd
+#  sudo mount /dev/mmcblk0p1 /mnt/sd
+#  ls /mnt/sd
+#  cd /mnt/sd/DCIM ~/
+#  cd /mnt/sd/DCIM .
+#  cp /mnt/sd/DCIM .
+#  cp -r /mnt/sd/DCIM .
+#  ls
+#  tmux attatch-session t 0
+#  tmux attach-session 0
+#  tmux attach-session 
+#  yap -S intel-media-sdk intel-media-driver intel-compute-runtime libva-mesa-driver 
+#  yay -S intel-media-sdk intel-media-driver intel-compute-runtime libva-mesa-driver 
+#  yay -S reflector
+#  yay -S pacman-mirrorlist
+#  pacman-mirrorlist
+#  pacman-mirrors -i -c China -m rank
+#  sudo pacman-mirrors -i -c China -m rank
+#  sudo reflector
+#  where zsh
+#  ls
+#  ln -sf /usr/bin/nvim /bin/vi
+#  sudo ln -sf /usr/bin/nvim /bin/vi
+#  vi
+#  vim
+#  sudo vi
+#  sudo vi /etc/pacman.d/mirrorlist
+#  sudo systemctl enable jellyfin
+#  sudo systemctl start jellyfin.service
+#  ls
+#  mount
+#  sudo vi /etc/fstab
+#  sudo vi /etc/passwd
+#  ls
+#  ls /
+#  mkdir /media
+#  sudo mkdir /media
+#  cd /media
+#  ls
+#  vi /etc/fstab
+#  mkdir /media/Movies
+#  sudo chmod 777 /media
+#  ls
+#  cd /media
+#  mkdir Movies
+#  mkdir Music
+#  mkdir TV
+#  mkdir Downloads
+#  mkdir FariyTale
+#  mkdir Photos
+#  mkdir 'Variety Shows'
+#  ls
+#  ls -al
+#  sudo vi /etc/fstab
+#  ls
+#  ssh-keygen
+#  ls
+#  cd ~/.ssh
+#  ls
+#  exit
+#  ls
+#  rm -rf DCIM
+#  ls
+#  ls /media
+#  ls
+#  cd /media
+#  ls Downloads
+#  ls -al
+#  cd Downloads
+#  sudo su
+#  ls /media/Downloads
+#  cd /media
+#  ls
+#  cd Downloads
+#  ls Downloads
+#  chown tang Downloads
+#  sudo chown tang Downloads
+#  cd Downloads
+#  ls
+#  ls Downloads
+#  cd Downloads
+#  ls
+#  cd .
+#  cd ..
+#  ls
+#  cd FariyTale
+#  sudo su
+#  yay -S genfstab
+#  pacman -Syuu
+#  sudo pacman -Syuu
+#  ls
+#  cd ..
+#  ls
+#  mkdir muyi
+#  ls
+#  sudo mkdir muyi
+#  sudo chown tang muyi
+#  cd muyi
+#  ls
+#  cd ..
+#  mount 10.0.0.8:/ /home/muyi
+#  sudo mount 10.0.0.8:/ /home/muyi
+#  systemctl daemon-reload\
+#  systemctl daemon-reload
+#  sudo systemctl daemon-reload
+#  sudo mount 10.0.0.8:/ /home/muyi
+#  sudo dmesg
+#  sudo mount 10.0.0.8:/Pt /media/Movies
+#  reboot
+#  sudo reboot
+#  sudo mount 10.0.0.8:/Pt /media/Movies
+#  yay -S nfs-utils
+#  sudo vi /etc/fstab
+#  sudo reboot
+#  ls /media/Movies
+#  sudo vi /etc/fstab
+#  ls
+#  sudo mount -a
+#  ls
+#  cd /media
+#  ls
+#  cd Downloads
+#  ls
+#  sudo reboot
+#  ls 
+#  ls /me
+#  cd /media
+#  ls
+#  cd Downloads
+#  ls
+#  cd ..
+#  ls
+#  cd FariyTale
+#  ls
+#  cd ..
+#  ls
+#  cd Movies
+#  ls
+#  cd ..
+#  ls
+#  ls Music
+#  ls
+#  ls Photos
+#  ls TV
+#  ls
+#  ls Variety\ Shows
+#  ls
+#  ls Downloads
+#  ls
+#  cd ..
+#  ls
+#  cd media
+#  ls
+#  sudo vi /etc/locale.conf
+#  lspci -nn | egrep -i "3d|display|vga"
+#  sudo yay -S linux-firmware
+#  vi /etc/default/grub
+#  sudo vi /etc/default/grub
+#  ls
+#  sudo mount /dev/nvme0n1p6 /mnt/manjaro
+#  cd /mnt/manjaro
+#  ls
+#  vi etc/default/grub
+#  sudo cat /sys/kernel/debug/dri/0/gt/uc/guc_info
+#  sudo reboot
+#  lspci -nn | egrep -i "3d|display|vga
+#  cat /sys/kernel/debug/dri/0/gt/uc/guc_info
+#  sudo cat /sys/kernel/debug/dri/0/gt/uc/guc_info
+#  sudo cat /sys/kernel/debug/dri/0/gt/uc/huc_info
+#  sudo su
+#  sudo cat /sys/kernel/debug/dri/0/gt/uc/guc_info
+#  vi /etc/modprobe.d/i915.conf
+#  ls
+#  sudo  systemctl status jellyfin
+#  df -h /
+#  ls
+#  ls /b
+#  cd /b
+#  ls
+#  cd /boot
+#  ls
+#  ls /bin
+#  yay -S  \tmesa
+#  sudo vi /etc/mkinitcpio.conf
+#  sudo mkinitcpio -P
+#  sudo reboot
+#  sudo vi /etc/modprobe.d/i915.conf
+#  sudo mkinitcpio -P
+#  sudo reboot
+#  dmesg
+#  sudo dmesg
+#  sudo dmesg | grep huc
+#  sudo cat /sys/kernel/debug/dri/0/gt/uc/huc_info
+#  sudo cat /sys/kernel/debug/dri/0/gt/uc/guc_info
+#  history
+#  sudo vi /etc/mkinitcpio.conf
+#  modinfo -p i915
+#  yay -S jellyfin-ffmpeg
+#  yay -S jellyfin-ffmpeg5
+#  cd .c
+#  cd .cache
+#  ls
+#  cd yay
+#  ls
+#  rm -rf jellyfin-ffmpeg
+#  ls
+#  cd jellyfin-ffmpeg5
+#  ls
+#  cd jellyfin-ffmpeg
+#  ls
+#  cd branches
+#  ls
+#  cd ..
+#  ls config
+#  cd hooks
+#  ls
+#  cd .
+#  cd ..
+#  cd branches
+#  ls
+#  cd ..
+#  sudo vi /etc/hosts
+#  sudo reboot
+#  tmux
+#  yay -S jellyfin-ffmpeg5
+#  cd .cache/yay
+#  ls
+#  cd jellyfin-ffmpeg5
+#  ls
+#  cd src
+#  ls
+#  tmux attach-session
+#  cd jellyfin-ffmpeg5
+#  yay -S jellyfin-ffmpeg5
+#  where jellyfin-ffmpeg5
+#  ffmpeg
+#  where ffmpeg
+#  yay -S jellyfin-ffmpeg
+#  yay -S jellyfin-ffmpeg5
+#  cd /usr/lib
+#  ls
+#  jellyfin-ffmpeg/ffmpeg
+#  cd ..
+#  ls
+#  cd lib
+#  ls
+#  jellyfin-ffmpeg
+#  ls
+#  ff
+#  ffmpeg
+#  pwd
+#  ffmpeg
+#  pwd
+#  ffmpeg
+#  yay -S wqy-microhei
+#  yay -S adobe-source-han-serif-cn-fonts
+#  cd /usr/share/font
+#  cd /usr/share/fonts
+#  ls
+#  cd adobe-source-han-serif
+#  ls
+#  pwd
+#  yay -S intel_gpu_top
+#  ls
+#  adb
+#  yay -S adb
+#  adb connect 10.0.0.15
+#  adb
+#  adb connect 10.0.0.15
+#  ls
+#  adb install /media/Downloads/jellyfin-androidtv-v0.14.2-release-unsigned.apk
+#  ls /media/Downloads/jellyfin\n
+#  adb install /media/Downloads/jellyfin-androidtv-v0.14.2-release.aab
+#  adb install /media/Downloads/jellyfin-androidtv-v0.14.2-debug.apk
+#  adb install /media/Downloads/jellyfin-androidtv-v0.14.2-debug_th.apk
+#  yay -S intel-gpu-tools
+#  sudo intel_gpu_top
+#  adb install /media/Downloads/jellyfin-androidtv-v0.14.2-release.apk
+#  adb uninstall /media/Downloads/jellyfin-androidtv-v0.14.2-debug_th.apk
+#  adb shell pm list packages -f -3
+#  adb uninstall org.jellyfin.androidtv.debug
+#  adb shell pm list packages -f -3
+#  adb uninstall com.gitvdemo.video
+#  adb shell pm list packages -f -5
+#  adb shell pm list packages -f -3
+#  adb shell pm list packages -f -2
+#  adb shell pm list packages -f -1
+#  adb shell pm list packages -f -3
+#  adb shell pm list packages
+#  ls
+#  sudo systemctl restart jellyfin
+#  ls
+#  cd //usr/share/fonts/
+#  ls
+#  cd wenquanyi
+#  ls
+#  cd wqy-microhei
+#  ls
+#  pwd
+#  sudo systemctl restart jellyfin
+#  ls
+#  sudo reboot
+#  adb install /media/Downloads/qiyiguo_official12.8.2.apk
+#  adb connect 10.0.0.15
+#  adb install /media/Downloads/qiyiguo_official12.8.2.apk
+#  adb kill-server
+#  adb connect 10.0.0.15
+#  adb install /media/Downloads/qiyiguo_official12.8.2.apk
+#  ls
+#  pwd
+#  clear
+#  vi /etc/passwd
+#  cd /var/lib/jellyfin
+#  ls
+#  yay -S locales
+#  locale-gen en_US.UTF-8
+#  ls
+#  sz
+#  ls
+#  cpw
+#  ls
+#  vi .zshrc
+#  history
+#  ls
+#  cd /etc/pacman.d/
+#  ls
+#  cp mirrorlist mirrorlist.bk
+#  sudo cp mirrorlist mirrorlist.bk
+#  ls
+#  sudo vi mirrorlist
+#  sudo vi /etc/pacman.conf
+#  s
+#  pacman
+#  ls
+#  pac
+#  cd ~
+#  vi .zshrc
+#  sudo /etc/locale.conf
+#  sudo vi /etc/locale.conf
+#  locale-gen en_US.UTF-8
+#  sleep ls
+#  ls
+#  \tlss
+#  vi .zshrc
+#  source .zshrc
+#  sudo reboot
+#  ls
+#  locale -a
+#  sudo vi /etc/locale.conf
+#  sudo sudo locale-gen en_US.UTF-8
+#  locale -a
+#  sudo vi /etc/default/local
+#  rm -rf /etc/default/local
+#  sudo rm -rf /etc/default/local
+#  ls
+#  cd /etc
+#  ls
+#  sudo vi locale.gen
+#  locale-gen
+#  sudo locale-gen
+#  locale -a
+#  source ~/.zshrc
+#  ls
+#  vi /etc/locale.conf
+#  ls
+#  LSCOLORS
+#  ls
+#  clear
+#  sudo reboot
+#  locale -a
+#  ls
+#  pwd
+#  cls
+#  ls
+#  pwd
+#  cls
+#  clear
+#  sudo pacman -Syu
+#  sudo reboot
+#  ls
+#  pwd
+#  clear
+#  df -h /
+#  df 
+#  df -a
+#  df -hT /home
+#  df -m 
+#  ls
+#  pwd
+#  cls
+#  pu
+#  sudo pacman -Syuu
+#  sudo reboot
+#  ls
+#  pwd
+#  clear
+#  sudo pacman -Syuu
+#  paru -Sua
+#  ls
+#  pwd
+#  cls
+#  ls -al
+#  cd .android
+#  ls
+#  cd ..
+#  cd .cache
+#  ls
+#  cd paru
+#  ls
+#  cd ..
+#  ls
+#  cd yay
+#  ls
+#  cd jellyfin-ffmpeg5
+#  ls
+#  paru -Sua
+#  cd ~
+#  ls
+#  lspci\nxrandr --listproviders
+#  lspci
+#  xrandr --listproviders
+#  sudo xrandr --listproviders
+#  sudo dmesg
+#  upgrade_oh_my_zsh
+#  ls
+#  pwd
+#  cls
+#  clear
+#  yay -Syuu
+#  paru -Sua
+#  pacm -Syuu
+#  pacm -Syu
+#  sudo pacman -Syu
+#  ls
+#  yay -S timeshift
+#  pacman -Qqen
+#  pacman -Qqem
+#  yay -S dust
+#  dust
+#  cd /
+#  dust
+#  ls
+#  clear
+#  cn
+#  ls
+#  pwd
+#  clear
+#  pu
+#  sudo pacman -Syuu
+#  vim
+#  sudo pacman -R  vim-runtime
+#  paru -Sua
+#  ls
+#  pwd
+#  clar
+#  clear
+#  pwd
+#  ls
+#  pwd
+#  clear
+#  tmux
+#  ls
+#  sudo pacman -Syuu
+#  pacman -Rs
+#  sudo pacman -Rs
+#  sudo pacman -Rs vim
+#  sudo pacman pacman -Qdtq\n
+#  sudo pacman -Qdtq
+#  sudo pacman -Syu
+#  ls
+#  wpd
+#  pw
+#  clear
+#  ls
+#  sudo reboot
+#  lspci
+#  ls
+#  xrandr 
+#  sudo xrandr
+#  sudo reboot
+#  ls
+#  pwd
+#  sudo reboot
+#  sudo pacman -Syu
+#  yay -
+#  yay -S vim
+#  sudo pacman -Syu
+#  sudo reboot
+#  yay -Sua
+#  paru -Sua
+#  yay -Sua
+#  sudo pacman -Syu
+#  sudo reboot
+#  sudo pacman -Sua
+#  sudo pacman -Syu
+#  cd /boot/
+#  ls
+#  rm ''\''\n
+#  ls
+#  rm "'"
+#  ls
+#  vi "'"
+#  ls
+#  pwd
+#  cls
+#  clear
+#  ls
+#  df -h /
+#  ls
+#  pwd
+#  clear
+#  sudo pacman -Sua
+#  sudo pacman -Syu
+#  sudo reboot
+#  systemctl --type=service
+#  systemctl list-units --type=service
+#  clear
+#  systemctl list-units --type=service
+#  systemctl list-unit-files --type=service --state=enabled
+#  systemctl list-unit-files | grep enabled
+#  sudo exportfs -v
+#  df -h /
+#  df -h /media/Downloads
+#  systemctl list-unit-files | grep Downloads
+#  yay -S qbittorrent-nox
+#  python
+#  ls
+#  sudo systemctl enable qbittorrent-nox@.service
+#  sudo systemctl enable qbittorrent-nox.service
+#  sudo systemctl enable qbittorrent-nox
+#  sudo systemctl enable qbittorrent
+#  yay -S qbittorrent-enhanced-nox
+#  where qbittorrent-nox
+#  sudo useradd qBitorrent
+#  sudo useradd qbt
+#  ls
+#  vi /etc/passwd
+#  sudo vi /etc/passwd
+#  sudo userdel qbt
+#  ls
+#  vi /etc/passwd
+#  tmux
+#  0;10;1c
+#  ls
+#  exit
+#  tmux
+#  ls
+#  sudo su qbittorrent
+#  sudo vi /etc/passwd
+#  zsh
+#  exit
+#  ls
+#  which zsh
+#  sudo usermod -s /usr/bin/zsh qbittorrent
+#  sudo su qbittorrent
+#  sudo vi /etc/systemd/system/qbittorrent.service
+#  systemctl list-unit-files | grep Downloads
+#  systemctl list-unit-files | grep online
+#  systemctl list-unit-files | grep mount
+#  systemctl list-unit-files | grep mount | grep media
+#  sudo systemctl enable qbittorrent.service
+#  sudo systemctl start qbittorrent.service
+#  sudo su qbittorrent
+#  cd /media/Downloads
+#  mkdir a
+#  pwd
+#  cd ..
+#  ls
+#  cd Movies
+#  mkdir a
+#  ls
+#  sudo vi /etc/systemd/system/qbittorrent.service
+#  ls
+#  sudo reboot
+#  ls
+#  sudo systemctl start qbittorrent.service
+#  cd /media
+#  ls
+#  cd Movies
+#  ls
+#  pwd
+#  journalctl -b
+#  sudo journalctl -b
+#  sudo journalctl /usr/lib/systemd/systemd
+#  ls
+#  sudo reboot
+#  sudo systemctl start qbittorrent.service
+#  sudo journalctl -u qbittorrent.service
+#  sudo vi /etc/systemd/system/qbittorrent.service
+#  sudo reboot
+#  sudo journalctl -u qbittorrent.service
+#  sudo vi /etc/systemd/system/qbittorrent.service
+#  sudo journalctl -u network-online.target
+#  sudo  vi /etc/systemd/system/qbittorrent.service
+#  sudo journalctl -u nss-lookup.target
+#  sudo journalctl -u media-Downloads.mount
+#  ls
+#  sudo systemctl list-units 
+#  sudo systemctl list-units | grep mount
+#  sudo journalctl -u  media-Downloads.automount
+#  ls
+#  sudo reboot
+#  sudo journalctl -u  media-Downloads.automount
+#  sudo journalctl -u  media-Downloads.mount
+#  ls
+#  sudo vi /etc/systemd/system/qbittorrent.service
+#  sudo reboot
+#  sudo journalctl -u qbittorrent.service
+#  sudo journalctl -u  media-Downloads.mount
+#  ls
+#  yay -S nfs-common
+#  sudo vi /etc/fstab
+#  sudo journalctl -u NetworkManager-wait-online.service
+#  sudo systemctl enable NetworkManager-wait-online.service
+#  sudo reboot
+#  ls
+#  sudo journalctl -u  media-Downloads.mount
+#  sudo vi /etc/systemd/system/qbittorrent.service
+#  sudo systemctl enable media-Photos.automount
+#  sudo systemctl start qbittorrent.service
+#  systemctl daemon-reload
+#  sudo systemctl daemon-reload
+#  sudo systemctl start qbittorrent.service
+#  sudo reboot
+#  sudo vi /etc/systemd/system/onemin.timer
+#  sudo systemctl stop qbittorrent.service
+#  sudo mv /etc/systemd/system/onemin.timer /etc/systemd/system/qbittorrent.timer
+#  sudo systemctl enable qbittorrent.timer
+#  sudo systemctl disable qbittorrent.service
+#  sudo systemctl start qbittorrent.timer
+#  sudo reboot
+#  ls
+#  sudo journalctl -u qbittorrent.timer
+#  ls
+#  sudo vi /etc/systemd/system/qbittorrent.timer
+#  vi /etc/systemd/system/qbittorrent.service
+#  sudo reboot
+#  yay -R qbittorrent-enhanced-nox
+#  yay -S qbittorrent-nox
+#  ls
+#  sudo reboot
+#  pwd
+#  clear
+#  sudo journalctl -u qbittorrent.timer
+#  where qbittorrent-nox
+#  ls
+#  cd .ssh
+#  ls
+#  vi id_rsa.pub
+#  ls
+#  cd ~
+#  ls
+#  mkdir Git
+#  ls
+#  cd Git
+#  ls
+#  git clone git@github.com:txiangyang/SetupEnv.git
+#  ls
+#  cd SetupEnv
+#  ls
+#  mkdir archlinux
+#  cd archlinux
+#  ls
+#  ln /etc/systemd/system/qbittorrent.timer qbittorrent.timer
+#  ln -s /etc/systemd/system/qbittorrent.timer qbittorrent.timer 
+#  ls 
+#  vi qbittorrent.timer
+#  ln -s /etc/systemd/system/qbittorrent.service qbittorrent.service
+#  ls
+#  git config user.name txiangyang
+#  git config user.email xiangyangtang@outlook.com
+#  git commit
+#  git add qbittorrent.*
+#  git commit -a
+#  git push
+#  ls
+#  sudo
+#  ls
+#  rm -rf qbittorrent.timer
+#  sudo ln /etc/systemd/system/qbittorrent.timer qbittorrent.timer
+#  ls
+#  vi qbittorrent.timer
+#  rm -f qbittorrent.service
+#  ls
+#  sudo ln /etc/systemd/system/qbittorrent.service qbittorrent.service
+#  ls
+#  git add qbittorrent.*
+#  git commit -a
+#  git push
+#  ls
